b61f61b0383478c90f4061231e3b47b22db36d6d
jsh <tmdgusya@naver.com>
2021-06-30 23:42:17 +0900

    feat: Create User Model And Admin Page

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """Custom User Models"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+
