ca2942a34e4771ad418cc35e7663d74803501e86
jsh <tmdgusya@naver.com>
2021-07-01 22:03:09 +0900

    feat: Creat User Model

--- users/models.py
+++ users/models.py
@@ -16,7 +16,27 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other")
     )
 
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREAN = "korean"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean")
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "usd"),
+        (CURRENCY_KRW, "krw")
+    )
+
     bio = models.TextField(default="", blank=True)
     avatar = models.ImageField(null=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(choices=LANGUAGE_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
 
