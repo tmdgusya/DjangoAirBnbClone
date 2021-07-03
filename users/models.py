01d3a642209b4e54c76735efd4840b187a342d29
jsh <tmdgusya@naver.com>
2021-07-03 23:31:26 +0900

    refactor: dateField 를 제외한 모든 Field Null=True 제거
    String field 는 Null 보다는 "" 으로 받을 수 있으므로 제거

--- users/models.py
+++ users/models.py
@@ -34,11 +34,11 @@ class User(AbstractUser):
         (CURRENCY_KRW, "krw")
     )
 
-    bio = models.TextField(default="", blank=True)
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
-    birthday = models.DateField(null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True)
-    currency = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
+    currency = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
     superhost = models.BooleanField(default=False)
 
