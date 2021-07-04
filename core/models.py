7d293a0fe2f78a294d97561e64544cb34f083226
tmdgusya <tmdgusya@naver.com>
2021-07-04 16:48:45 +0900

    feat: room models 생성

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampModel(models.Model):
 
     """TimeStampModel Definition"""
 
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract: True
--- rooms/models.py
+++ rooms/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from django_countries.fields import CountryField
 from core import models as core_model
+from users import models as user_model
 
 
 class Rooms(core_model.TimeStampModel):
 
     """Room Model Definition"""
 
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bath = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_model.User, on_delete=models.CASCADE)
